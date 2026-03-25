bf7b6c7d558551a2859cda14ddc4104e345a0e6e
i_lopez_gazpio <inigo.lopez@ehu.eus>
2026-03-25 15:48:00 +0100

    Version a utilizar con la 4060

---
+++ src/intrinsic_curiosity_based_driving/train_charged_models.py
@@ -0,0 +1,419 @@
+from gym_liftoff.envs.liftoff_env import Liftoff
+
+from src.utils.datasets import LMDBIntrinsicCuriosityDataset
+from .ensemble import EnsembleModel, StateEncoder, StateDecoder
+from .policy import Actor, Critic, compute_gae
+from .lmdb_utils import LMDBWriter
+from queue import Queue
+from threading import Thread
+from src.utils.datasets import IntrinsicCuriosityDataset, PPODataset
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from pathlib import Path
+import torch.nn.functional as F
+import torch
+from torchvision import transforms
+import pickle
+import lmdb
+
+current_dir = Path(__file__).resolve().parent
+lmdb_path = current_dir / "lmdb_episode"
+
+def save_last_episode(episode:int):
+    with last_episode_saving_path.open("w") as f:
+        f.write(str(episode))
+
+def read_last_episode():
+    if not last_episode_saving_path.exists():
+        return -1
+    with last_episode_saving_path.open("r") as f:
+        return int(f.read())
+
+def optimizer_to(optim, device):
+    for state in optim.state.values():
+        for k, v in state.items():
+            if torch.is_tensor(v):
+                state[k] = v.to(device)
+
+models_dir = current_dir / "models"
+last_episode_saving_path = models_dir / "last_episode.txt"
+models_dir.mkdir(exist_ok=True, parents=True)
+
+env = Liftoff(continuous_action_mode=True)
+
+print("Observation space:", env.observation_space)
+print("Action space:", env.action_space)
+
+NUM_EPISODES = 10000
+NUM_ENSEMBLE_MODELS = 8
+LATENT_DIM = 256
+ACTION_DIM = 4
+BATCH_SIZE = 10
+QUEUE_MAX = 500
+LAMBDA = 1 # weighs the intrinsic reward in the total reward
+PPO_EPOCHS = 4
+PPO_BATCH = 10
+
+# =================
+# Models
+# =================
+
+encoder = StateEncoder(latent_dim=LATENT_DIM)
+decoder = StateDecoder(latent_dim=LATENT_DIM)
+actor = Actor(action_dim=ACTION_DIM)
+critic = Critic(action_dim=ACTION_DIM)
+ensemble = [EnsembleModel(LATENT_DIM, ACTION_DIM) for _ in range(NUM_ENSEMBLE_MODELS)]
+
+checkpoint = None
+last_episode = 0
+try:
+    print("Trying to charge models")
+    last_episode = read_last_episode()
+    checkpoint = torch.load(models_dir / f"models_optimizers_{last_episode}.pth")
+    print("Models Charged, starting from episode", last_episode)
+    encoder.load_state_dict(checkpoint["models"]["encoder"])
+    decoder.load_state_dict(checkpoint["models"]["decoder"])
+    actor.load_state_dict(checkpoint["models"]["actor"])
+    critic.load_state_dict(checkpoint["models"]["critic"])
+    for model, state_dict in zip(ensemble, checkpoint["models"]["ensemble"]):
+        model.load_state_dict(state_dict)
+except FileNotFoundError:
+    pass
+
+
+# =================
+# Optimizers
+# =================
+
+learning_rate = 1e-3
+encoder_opt = Adam(encoder.parameters(), lr = learning_rate)
+decoder_opt = Adam(decoder.parameters(), lr = learning_rate)
+actor_opt = Adam(actor.parameters(), lr = 1e-4)
+critic_opt = Adam(critic.parameters(), lr = learning_rate)
+ensemble_opt = [Adam(ens.parameters(), lr = learning_rate) for ens in ensemble]
+
+if checkpoint:
+    encoder_opt.load_state_dict(checkpoint["optimizers"]["encoder"])
+    decoder_opt.load_state_dict(checkpoint["optimizers"]["decoder"])
+    actor_opt.load_state_dict(checkpoint["optimizers"]["actor"])
+    critic_opt.load_state_dict(checkpoint["optimizers"]["critic"])
+    for opt, state in zip(ensemble_opt, checkpoint["optimizers"]["ensemble"]):
+        opt.load_state_dict(state)
+
+# =================
+# Devices
+# =================
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cpu = torch.device("cpu")
+
+# =================
+# Initialize Worker and LMDB
+# =================
+
+writer = LMDBWriter(lmdb_path=str(lmdb_path))
+
+
+# =================
+# Image Normalization
+# =================
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+
+# =================
+# Training
+# =================
+
+torch.cuda.empty_cache()
+
+for episode in range(last_episode, NUM_EPISODES):
+    print(f"\n===== EPISODE {episode} =====")
+    prev_actions = []
+    dones = []
+    log_probs = []
+    env_rewards = []
+    infos = []
+    actor = actor.to(device)
+
+    writer.open()
+    writer.clear_database()
+
+    # reset del env
+    obs, _ = env.reset()
+    done = False
+
+    previous_action = torch.zeros((1, ACTION_DIM), dtype = torch.float32).to(device)
+    step = 0
+    torch.cuda.empty_cache()
+    while not done:
+        # convertir obs a tensor
+        obs_tensor = torch.from_numpy(obs).float() / 255.0
+        obs_tensor = normalize(obs_tensor)
+        obs_tensor = obs_tensor.unsqueeze(0)
+
+        # acción del policy PPO
+        action, log_prob = actor.sample_action(obs_tensor.to(device), previous_action)
+
+        # ejecutar acción
+        next_obs, reward, terminated, truncated, info = env.step(action.squeeze(0).detach().cpu().numpy())
+        done = terminated or truncated
+
+        #print("Action:", action)
+        # almacenar info para entrenamiento posterior
+        t = step
+        data_step = {
+            "img": obs_tensor.squeeze(0).to(cpu),
+            "action": action.to(cpu),
+            "reward": reward,
+            "step": t
+        }
+
+        writer.put(data_step)
+
+        prev_actions.append(previous_action.detach().to(cpu))
+        env_rewards.append(reward)
+        dones.append(done)
+        log_probs.append(log_prob.detach().to(cpu))
+
+        previous_action = action.detach()
+
+        infos.append(info)
+
+
+        # actualizar obs
+        obs = next_obs
+        step += 1
+
+
+    last_step = {"img": obs, "step": step}
+    writer.put(last_step)
+    writer.close()
+
+    actor.to(cpu)
+    del previous_action
+    del reward
+    del info
+    del next_obs
+    del obs
+    torch.cuda.empty_cache()
+
+    print(f"Episode finished in {step} steps, total env reward: {sum(env_rewards):.3f}")
+
+    intrinsic_curiosity_dataset = LMDBIntrinsicCuriosityDataset(lmdb_path=str(lmdb_path))
+    intrinsic_curiosity_loader = DataLoader(intrinsic_curiosity_dataset, batch_size = BATCH_SIZE)
+
+
+    ppo_dataset = PPODataset(log_probs = log_probs, dones= dones, past_actions=prev_actions)
+    ppo_loader = DataLoader(ppo_dataset, batch_size = BATCH_SIZE)
+
+
+    """
+    encoder = encoder.to(device)
+    decoder = decoder.to(device)
+    ensemble = [e.to(device) for e in ensemble]
+    critic = critic.to(device)
+    """
+
+    encoder = encoder.to(device)
+    optimizer_to(encoder_opt, device)
+    decoder = decoder.to(device)
+    optimizer_to(decoder_opt, device)
+    for e, opt in zip(ensemble, ensemble_opt):
+        e.to(device)
+        optimizer_to(opt, device)
+    critic = critic.to(device)
+
+
+    final_rewards = torch.Tensor().to(device)
+    values = torch.Tensor().to(device)
+
+    for intrinsic_batch, ppo_batch in zip(intrinsic_curiosity_loader, ppo_loader):
+        obs, act, env_reward, next_obs = [x.to(device) for x in intrinsic_batch]
+        _, _, prev_action, _, _, _, _ = [x.to(device) for x in ppo_batch]
+
+
+        z = encoder(obs)
+        z_next = encoder(next_obs)
+
+        reconstruct_obs = decoder(z)
+        reconstruction_loss = F.mse_loss(reconstruct_obs, obs)
+
+        ensemble_losses = []
+        ensemble_preds = []
+        for nsbl in ensemble:
+            pred_next_z = nsbl(z, act)
+            loss = F.mse_loss(pred_next_z, z_next.detach())
+            ensemble_losses.append(loss)
+            ensemble_preds.append(pred_next_z)
+
+        ensemble_loss = torch.stack(ensemble_losses).mean()
+        #print(ensemble_preds)
+        print(f"Reconstruction loss: {reconstruction_loss.item():.4f}, Ensemble loss: {ensemble_loss.item():.4f}")
+
+        for nsbl in ensemble:
+            for p in nsbl.parameters():
+                p.requires_grad = False
+
+
+        # Encoder Backprop
+        encoder_opt.zero_grad()
+        (total_encoder_loss := reconstruction_loss + torch.clamp(ensemble_loss, 0, 10)).backward()
+        encoder_opt.step()
+
+        for nsbl in ensemble:
+            for p in nsbl.parameters():
+                p.requires_grad = True
+
+
+        reconstruct_obs2 = decoder(encoder(obs).detach())
+        decoder_loss = F.mse_loss(reconstruct_obs2, obs)
+        # Decoder Backprop
+        decoder_opt.zero_grad()
+        decoder_loss.backward()
+        decoder_opt.step()
+        z_detached = z.detach()
+        # Ensemble models backprop
+        print(z_next)
+        for nsbl, optimizer in zip(ensemble, ensemble_opt):
+
+            pred = nsbl(z_detached, act.detach())
+            mask = torch.bernoulli(0.8 * torch.ones(pred.size(0), device=pred.device)).bool()
+
+            if mask.sum() == 0:
+                mask[:] = True
+
+            loss = F.mse_loss(pred[mask], z_next[mask].detach())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        """
+        ir = torch.mean(torch.var(ensemble_preds, dim=1, unbiased=False), dim=1)
+        ir = (ir - ir.mean()) / (ir.std() + 1e-8)
+        ir = ir.unsqueeze(-1) # [batch, 1]
+        """
+
+        ensemble_preds_ir = torch.stack(ensemble_preds)
+
+        ir = torch.var(ensemble_preds_ir, dim=0, unbiased=False)
+        ir = ir.mean(dim=1)  # [batch]
+
+        print(f"Intrinsic reward mean: {ir.mean().item():.4f}, std: {ir.std().item():.4f}")
+        # TODO: Ponderar si es necesario
+        total_reward = env_reward + LAMBDA*ir
+
+        if total_reward.numel() > 1:
+            reward_std = total_reward.std()
+            if reward_std > 1e-8:
+                total_reward = (total_reward - total_reward.mean()) / (reward_std + 1e-8)
+            else:
+                total_reward = total_reward - total_reward.mean()  # Solo centrar si no hay varianza
+        else:
+            total_reward = total_reward - total_reward.mean()
+
+        reward = total_reward
+        value = critic(obs, prev_action)
+
+        final_rewards = torch.cat([final_rewards, reward])
+        values = torch.cat([values, value.detach()])
+
+        torch.cuda.empty_cache()
+    with torch.no_grad():
+        last_value = critic(obs[-1].unsqueeze(0), act[-1].unsqueeze(0)).detach()
+    advantages, returns = compute_gae(final_rewards.detach(), values.detach(), dones)
+
+    torch.cuda.empty_cache()
+    encoder = encoder.to(cpu)
+    decoder = decoder.to(cpu)
+    optimizer_to(encoder_opt, cpu)
+    optimizer_to(decoder_opt, cpu)
+    for e, opt in zip(ensemble, ensemble_opt):
+        e.to(cpu)
+        optimizer_to(opt, cpu)
+
+    torch.cuda.empty_cache()
+
+    actor.to(device)
+    optimizer_to(actor_opt, device)
+    optimizer_to(critic_opt, device)
+    clip_eps = 0.2
+    ppo_dataset = PPODataset(log_probs, dones, prev_actions, rewards=final_rewards, advantages=advantages, values=values, returns=returns)
+    ppo_loader = DataLoader(ppo_dataset, batch_size = PPO_BATCH)
+
+    intrinsic_curiosity_loader = DataLoader(intrinsic_curiosity_dataset, batch_size = PPO_BATCH)
+    for _ in range(PPO_EPOCHS):
+        for intrinsic_batch, ppo_batch in zip(intrinsic_curiosity_loader, ppo_loader):
+            b_obs, b_acts, _, _ = [x.to(device) for x in intrinsic_batch]
+            old_log_probs, _, b_past_acts, _, b_adv, b_val, b_return = [x.to(device) for x in ppo_batch]
+
+            b_acts = b_acts.squeeze(1).requires_grad_(False)
+            b_obs = b_obs.requires_grad_(False)
+            b_past_acts = b_past_acts.squeeze(1)
+            b_new_probs = actor.get_log_probs(b_obs, b_past_acts, b_acts)
+
+            ratio = torch.exp(b_new_probs - old_log_probs.detach())
+            ratio = torch.clamp(ratio, 0, 10)
+            surr1 = ratio*b_adv
+            surr2 = torch.clamp(ratio, 1.0 - clip_eps, 1.0 + clip_eps) * b_adv
+
+            #print("new_log_probs:", b_new_probs.min().item(), b_new_probs.max().item())
+            #print("old_log_probs:", old_log_probs.min().item(), old_log_probs.max().item())
+            #print("diff:", (b_new_probs - old_log_probs).min().item(), (b_new_probs - old_log_probs).max().item())
+
+            actor_loss = -torch.mean(torch.min(surr1, surr2))
+
+            actor_opt.zero_grad()
+            actor_loss.backward()
+            torch.nn.utils.clip_grad_norm_(actor.parameters(), max_norm=0.5)
+            actor_opt.step()
+            value_pred = critic(b_obs, b_past_acts.detach())
+            critic_loss = F.mse_loss(value_pred, b_return)
+            critic_opt.zero_grad()
+            critic_loss.backward()
+            torch.nn.utils.clip_grad_norm_(critic.parameters(), max_norm=0.5)
+            critic_opt.step()
+            print(f"PPO epoch actor_loss: {actor_loss.item():.4f}, critic_loss: {critic_loss.item():.4f}")
+
+    critic = critic.to(cpu)
+    optimizer_to(actor_opt, cpu)
+    optimizer_to(critic_opt, cpu)
+    del final_rewards
+    del dones
+    del values
+
+    torch.cuda.empty_cache()
+
+    if episode % 100 == 0 and episode != 0:
+        all_models = {
+            "models": {
+                "encoder": encoder.state_dict(),
+                "decoder": decoder.state_dict(),
+                "actor": actor.state_dict(),
+                "critic": critic.state_dict(),
+                "ensemble": [m.state_dict() for m in ensemble]
+            },
+            "optimizers": {
+                "encoder": encoder_opt.state_dict(),
+                "decoder": decoder_opt.state_dict(),
+                "actor": actor_opt.state_dict(),
+                "critic": critic_opt.state_dict(),
+                "ensemble": [opt.state_dict() for opt in ensemble_opt]
+            }
+        }
+        torch.save(all_models, models_dir / f"models_optimizers_{episode}.pth")
+        save_last_episode(episode=episode)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
